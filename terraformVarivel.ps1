7446ce256347507a7080d49a123dffe7665908a2
Gabriel Peixoto <g.martins.peixoto@avanade.com>
2020-04-14 12:56:19 -0300

    Criando AKS com terrafoms

---
+++ terraformVarivel.ps1
@@ -0,0 +1,8 @@
+
+Set-Variable -Name "ARM_SUBSCRIPTION_ID" -Value ""
+Set-Variable -Name "TF_VAR_client_id" -Value ""
+Set-Variable -Name "TF_VAR_client_secret" -Value ""
+Set-Variable -Name "ARM_TENANT_ID" -Value ""
+
+# Not needed for public, required for usgovernment, german, china044eafaf-2c9d-4092-893a-cc15c32e5ba3
+Set-Variable -Name "ARM_ENVIRONMENT" -Value "public"
